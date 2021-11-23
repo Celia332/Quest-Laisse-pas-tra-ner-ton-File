349e249384e437c1a06e1197144815e2cd71ddf8
Celia332 <83719415+Celia332@users.noreply.github.com>
2021-11-23 14:51:46 +0100

    Update Form.php

--- Form.php
+++ Form.php
@@ -1,4 +1,32 @@
 
+if($_SERVER['REQUEST_METHOD'] === "POST"){
+
+    $uploadDir = 'public/uploads/';
+    $uploadFile = $uploadDir . basename($_FILES['image']['name']);
+
+    $extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+    $extensions_ok = ['jpg', 'webp'];
+    $maxFileSize = 100000;
+
+
+    if (!in_array($extension, $extensions_ok)) {
+        echo $errors[] = 'Sélectionner une image jpg, wepb, ou jpeg !';
+    }
+
+    elseif (file_exists($_FILES['image']['tmp_name']) && filesize($_FILES['image']['tmp_name']) > $maxFileSize) {
+    echo $errors[] = "Le taille du fichier est inférieur à 1 M0 !";
+    }
+
+    else {
+        $uploadFile = uniqid('', true) . '.' . $extension;
+        move_uploaded_file($_FILES['avatar']['tmp_name'], $uploadFile);
+        echo 'L'image a bien été envoyée';
+        echo '<img src="' . $uploadFile . '">';
+        echo $_POST['firstname']. PHP_EOL;
+        echo $_POST['lastname']. PHP_EOL;
+        echo $_POST['age']. 'ans';
+    }
+}
 
 <form method="post" enctype="multipart/form-data">
 
@@ -29,16 +57,7 @@ if($_SERVER['REQUEST_METHOD'] === "POST"){
     $maxFileSize = 100000;
 
 
-    if (!in_array($extension, $extensions_ok)) {
-        $errors[] = 'Sélectionner une image jpg, wepb, ou jpeg !';
-    }
-
-    if (file_exists($_FILES['image']['tmp_name']) && filesize($_FILES['image']['tmp_name']) > $maxFileSize) {
-        $errors[] = "Le taille du fichier est inférieur à 1 M0 !";
-    }
-
-    var_dump($_FILES);
-}
+ 
 
 
 
